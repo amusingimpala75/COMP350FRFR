39fd67f46a882113ce4ac722bc9ba4c85eb67406
Hudson Hadley <hudsonhadley@yahoo.com>
2026-02-23 18:05:12 -0500

    Adding function to load the data in from a json file containing courses. Returns an ArrayList of courses that match what is in the json

--- backend/src/main/java/edu/gcc/hallmonitor/Main.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Main.java
@@ -1,6 +1,11 @@
 package edu.gcc.hallmonitor;
 
 import io.javalin.Javalin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +13,23 @@ public class Main {
     }
 
     public static void run(int port) {
+        try {
+            List<Course> courses = loadData("src/main/resources/courses.json");
+        } catch (IOException ioe) {
+            System.err.println("IO Exception occured: " + ioe.getMessage());
+        }
+
         Javalin.create(cfg -> { cfg.staticFiles.add("public"); })
                 .get("/", ctx -> ctx.result("Hello, world"))
                 .start(port);
+    }
+
+    public static List<Course> loadData(String coursesFilename) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode root = mapper.readTree(new File(coursesFilename));
+        JsonNode classesNode = root.get("classes"); // Grab the courses array inside the json
+
+        return mapper.readerForListOf(Course.class).readValue(classesNode);
     }
 }
