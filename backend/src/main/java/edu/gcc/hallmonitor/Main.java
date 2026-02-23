b7b7326e8f4df16fa6b049936841cd6de7129a99
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-02-23 14:15:07 -0500

    [jj-spr] initial version
    Created using jj-spr 0.1.0

--- backend/src/main/java/edu/gcc/hallmonitor/Main.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Main.java
@@ -1,11 +1,15 @@
 package edu.gcc.hallmonitor;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-
+        run(7070);
     }
 
-    public static void run() {
-
+    public static void run(int port) {
+        Javalin.create(cfg -> { cfg.staticFiles.add("public"); })
+                .get("/", ctx -> ctx.result("Hello, world"))
+                .start(port);
     }
 }
