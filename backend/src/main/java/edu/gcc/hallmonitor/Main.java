afc989f8d1849da7a112610272f52ef35ed57378
eipmavinna <146777395+eipmavinna@users.noreply.github.com>
2026-02-24 13:56:39 -0500

    Moved the loadData method into Search and added code so the first search loads all course data into a static variable inside of Search.

--- backend/src/main/java/edu/gcc/hallmonitor/Main.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Main.java
@@ -1,13 +1,8 @@
 package edu.gcc.hallmonitor;
 
 import io.javalin.Javalin;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -15,30 +10,10 @@ public class Main {
     }
 
     public static void run(int port) {
-        try {
-            List<Course> courses = loadData("courses.json");
-        } catch (FileNotFoundException fnfe) {
-            System.err.println("File not found: " + fnfe.getMessage());
-        } catch (IOException ioe) {
-            System.err.println("IO Exception occurred: " + ioe.getMessage());
-        }
 
         Javalin.create(cfg -> { cfg.staticFiles.add("public"); })
                 .get("/", ctx -> ctx.result("Hello, world"))
                 .start(port);
     }
 
-    public static List<Course> loadData(String coursesFilename) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        URL jsonURL = Main.class.getResource(String.format("/%s", coursesFilename));
-        if (jsonURL == null) {
-            throw new FileNotFoundException(String.format("Could not find '%s' in resources directory", coursesFilename));
-        }
-
-        JsonNode root = mapper.readTree(jsonURL);
-        JsonNode classesNode = root.get("classes"); // Grab the courses array inside the json
-
-        return mapper.readerForListOf(Course.class).readValue(classesNode);
-    }
 }
--- backend/src/main/java/edu/gcc/hallmonitor/Search.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Search.java
@@ -1,7 +1,14 @@
 package edu.gcc.hallmonitor;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Search {
 
@@ -9,6 +16,7 @@ public class Search {
     private ArrayList<Filter> filterList;
     private ArrayList<Course> searchResults;
     private ArrayList<Course> matchResults;
+    private static List<Course> allCourses; //intialized on the first search, shared across all searches. or constructor could call an init method
 
     public void applyFilter(Filter filter) {
 
@@ -19,15 +27,49 @@ public class Search {
     }
 
     public ArrayList<Course> getMatchResults() {
-        return null;
+        //TODO: make a /results route and use this method to convert into json
+        return matchResults;
     }
 
     //The constructors will search the db for the appropriate courses
     public Search(String searchQuery) {
+        this.searchQuery = searchQuery;
+        //if this is the first search, initialize the allCourses list
+        if(allCourses == null){
+            try {
+                allCourses = loadData("courses.json");
+            } catch (FileNotFoundException fnfe) {
+                System.err.println("File not found: " + fnfe.getMessage());
+            } catch (IOException ioe) {
+                System.err.println("IO Exception occurred: " + ioe.getMessage());
+            }
+        }
+
+
+        //TODO: parse query, get terms
+
+        //TODO: order (add to) searchResults based on what most closely matches the terms
+
+        //TODO: matchResults = searchResults since there are no filters
+        //matchResults = searchResults;
+
+        //for now:
+        matchResults = new ArrayList<>(allCourses);
+
 
     }
 
     public Search(String searchQuery, ArrayList<Filter> filterList) {
+        //if this is the first search, initialize the allCourses list
+        if(allCourses == null){
+            try {
+                allCourses = loadData("courses.json");
+            } catch (FileNotFoundException fnfe) {
+                System.err.println("File not found: " + fnfe.getMessage());
+            } catch (IOException ioe) {
+                System.err.println("IO Exception occurred: " + ioe.getMessage());
+            }
+        }
 
     }
 
@@ -36,4 +78,18 @@ public class Search {
     }
 
 
+
+    public static List<Course> loadData(String coursesFilename) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        URL jsonURL = Main.class.getResource(String.format("/%s", coursesFilename));
+        if (jsonURL == null) {
+            throw new FileNotFoundException(String.format("Could not find '%s' in resources directory", coursesFilename));
+        }
+
+        JsonNode root = mapper.readTree(jsonURL);
+        JsonNode classesNode = root.get("classes"); // Grab the courses array inside the json
+
+        return mapper.readerForListOf(Course.class).readValue(classesNode);
+    }
 }
