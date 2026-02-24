1da2a7a3c732aca157096f591cf0c8375ac6a533
Hudson Hadley <hudsonhadley@yahoo.com>
2026-02-23 23:32:20 -0500

    Updating Main so that the courses json is not in a static position. This way we can run a jar from something other than the backend folder since it will access the resources as they are embedded in the jar.

--- backend/src/main/java/edu/gcc/hallmonitor/Main.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Main.java
@@ -3,8 +3,10 @@ package edu.gcc.hallmonitor;
 import io.javalin.Javalin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
-import java.io.File;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 public class Main {
@@ -14,9 +16,11 @@ public class Main {
 
     public static void run(int port) {
         try {
-            List<Course> courses = loadData("src/main/resources/courses.json");
+            List<Course> courses = loadData("courses.json");
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("File not found: " + fnfe.getMessage());
         } catch (IOException ioe) {
-            System.err.println("IO Exception occured: " + ioe.getMessage());
+            System.err.println("IO Exception occurred: " + ioe.getMessage());
         }
 
         Javalin.create(cfg -> { cfg.staticFiles.add("public"); })
@@ -27,7 +31,12 @@ public class Main {
     public static List<Course> loadData(String coursesFilename) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
-        JsonNode root = mapper.readTree(new File(coursesFilename));
+        URL jsonURL = Main.class.getResource(String.format("/%s", coursesFilename));
+        if (jsonURL == null) {
+            throw new FileNotFoundException(String.format("Could not find '%s' in resources directory", coursesFilename));
+        }
+
+        JsonNode root = mapper.readTree(jsonURL);
         JsonNode classesNode = root.get("classes"); // Grab the courses array inside the json
 
         return mapper.readerForListOf(Course.class).readValue(classesNode);
