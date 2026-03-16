8ad1d5450cd9f4df35e00af60187a8eeb74f534d
Hudson Hadley <hudsonhadley@yahoo.com>
2026-03-16 16:11:50 -0400

    Registering another route so that search results are saved. Search object is initially null since no search has been made

--- backend/src/main/java/edu/gcc/hallmonitor/SearchController.java
+++ backend/src/main/java/edu/gcc/hallmonitor/SearchController.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import io.javalin.Javalin;
 
 public class SearchController {
-    private static Search search = new Search("");
+    private static Search search = null;
 
     public static void registerRoutes(Javalin app) {
         app.get("/search", ctx -> ctx.html(
@@ -21,15 +21,19 @@ public class SearchController {
 
 
         app.post("/search", ctx -> {
-            if (!ctx.body().equals(search.query())) {
+            if (search == null || !ctx.body().equals(search.query())) {
                 String query = ctx.body();
                 search = new Search(query);
             }
             ctx.json(search.getMatchResults());
         });
 
+        app.get("/search/results", ctx -> {
+            ctx.json(search.getMatchResults());
+        });
+
         // [TODO] there should be a better way to do this
-        app.get("/courses", ctx -> {
+        app.get("/courses", ctx -> { // currently in use for filters
             // create a Search object with empty query to get all courses
             // pls keep this so my frontend works -Luca
             Search allCoursesSearch = new Search("");
