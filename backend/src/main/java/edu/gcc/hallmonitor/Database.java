69e24e32b7466e4139c5747d836e7a68085cb9b8
Hudson Hadley <hudsonhadley@yahoo.com>
2026-04-13 17:50:49 -0400

    Adding dependency to actually get environment variables

--- backend/src/main/java/edu/gcc/hallmonitor/Database.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Database.java
@@ -1,11 +1,13 @@
 package edu.gcc.hallmonitor;
 
 import java.sql.*;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Database {
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
