52b48b1e7b035f56da28f9434f4e4cda8379d6d5
Hudson Hadley <hudsonhadley@yahoo.com>
2026-04-18 15:45:17 -0400

    Adding isUser() method to establish if the user is in the database

--- backend/src/main/java/edu/gcc/hallmonitor/User.java
+++ backend/src/main/java/edu/gcc/hallmonitor/User.java
@@ -3,28 +3,69 @@ package edu.gcc.hallmonitor;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.*;
 
 public class User {
     private String username;
     private byte[] passwordHash;
     private int gradYear;
 
-    public User(String username, String password) throws NoSuchAlgorithmException {
+    public User() {
+
+    }
+
+    public User(String username, String password) throws IllegalArgumentException {
+        if (password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        } else if (username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
         this.username = username;
 
-        MessageDigest digest = MessageDigest.getInstance("sha256");
-        passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        try {
+            MessageDigest digest = MessageDigest.getInstance("sha256");
+            passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException ignored) {} // won't fail since sha256 is hardcoded
     }
 
     public void setGradYear(int gradYear) {
         this.gradYear = gradYear;
     }
 
-    public boolean isUser() {
-        return false;
+    public static User authenticate(String username, String password) throws Exception {
+        User user = new User(username, password);
+
+        // If they are not a user and they can't successfully be added, throw an error
+        if (!user.isUser() && !user.addUser()) {
+            throw new SecurityException(
+                    String.format("Unable to add user '%s'", username)
+            );
+        }
+
+        return user;
     }
 
-    public boolean addUser(String username, byte[] passwordHash) {
+    /**
+     * Checks to see if the user with username and password_hash are in the database
+     * @return if the username and password match a user in the database
+     * @throws SQLException if the connection fails
+     */
+    public boolean isUser() throws SQLException {
+        Connection conn = Database.getConnection();
+        // Get all the users that match the current user (should be at max 1)
+        PreparedStatement prepStatement = conn.prepareStatement(
+                "SELECT * FROM public.\"users\"" +
+                    "WHERE username = ? AND password_hash = ?"
+        );
+        prepStatement.setString(1, username);
+        prepStatement.setBytes(2, passwordHash);
+        ResultSet rs = prepStatement.executeQuery();
+
+        // If the result set is empty, return false. If the result set has a next, return true
+        return rs.next();
+    }
+
+    public boolean addUser() {
         return false;
     }
 }
