8debd4124f8b057c2d7de9c90aa2424b8b768aed
Hudson Hadley <hudsonhadley@yahoo.com>
2026-04-19 19:30:22 -0400

    Test should only verify a connection can be made

--- backend/src/test/java/edu/gcc/hallmonitor/DatabaseTest.java
+++ backend/src/test/java/edu/gcc/hallmonitor/DatabaseTest.java
@@ -3,18 +3,13 @@ package edu.gcc.hallmonitor;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DatabaseTest {
     @Test
     public void testDatabaseConnection() throws SQLException {
-        Connection conn = Database.getConnection();
-        Statement statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT * FROM public.\"users\"");
-        assertTrue(rs.next()); // Should have a result
+        assertNotNull(Database.getConnection());
     }
 }
