d1e1c4a85c6afd9941932156e5bf389032ad1281
Hudson Hadley <hudsonhadley@yahoo.com>
2026-04-19 00:04:51 -0400

    Removing dotenv. Docker needs to read env variables that are imported. Not ones found in a file

--- backend/src/main/java/edu/gcc/hallmonitor/Database.java
+++ backend/src/main/java/edu/gcc/hallmonitor/Database.java
@@ -4,13 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Database {
-    private static final Dotenv DOTENV = Dotenv.load();
-    private static final String URL = DOTENV.get("DB_URL");
-    private static final String USER = DOTENV.get("DB_USER");
-    private static final String PASSWORD = DOTENV.get("DB_PASSWORD");
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
